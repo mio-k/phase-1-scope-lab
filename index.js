5263430ef482b6a2f6314b89e114e86e0aa3757d
mio kataoka <mio.kanna@gmail.com>
2022-02-26 15:18:16 -0800

    commit tree e92eb73e38a6d17452e2bfff4d25bbe9bbd90d70

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+// Write your solution in this filve!
+var customerName = `bob`;
+
+function upperCaseCustomerName() {
+    console.log(customerName = customerName.toUpperCase());
+}
+function setBestCustomer() {
+    bestCustomer = `not bob`;
+}
+function overwriteBestCustomer(){
+    bestCustomer = `maybe bob`;
+}
+const leastFavoriteCustomer = `John`;
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = `Amir`;
+}
